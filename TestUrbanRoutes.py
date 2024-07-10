6fac980cb01c6a27767fa96792dcf26f5f3def12
lizettPinzonPaez <lizett1415@hotmail.com>
2024-07-10 18:22:45 -0500

    TestUrbanRoutes.py

--- TestUrbanRoutes.py
+++ TestUrbanRoutes.py
@@ -1,11 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-import time
-import data
 from UrbanRoutesPage import UrbanRoutesPage
+import data
+import time
+
 
 
 class TestUrbanRoutes:
@@ -21,8 +19,8 @@ class TestUrbanRoutes:
 # Configurar la dirección
     def test_set_route(self):
         self.driver.maximize_window()
+        self.driver.implicitly_wait(10)
         self.driver.get(data.urban_routes_url)
-        time.sleep(5)
         routes_page = UrbanRoutesPage(self.driver)
         address_from = data.address_from
         address_to = data.address_to
@@ -32,7 +30,6 @@ class TestUrbanRoutes:
         to_value = self.driver.find_element(*routes_page.to_field).get_attribute("value")
         assert from_value == address_from
         assert to_value == address_to
-        time.sleep(2)
 
 # Seleccionar la tarifa Comfort.
     def test_request_comfort_taxi(self):
@@ -40,30 +37,38 @@ class TestUrbanRoutes:
         routes_page.open_taxi_modal()
         routes_page.select_comfort_rate()
 
+
 # Rellenar el número de teléfono
     def test_add_phone_number(self):
         routes_page = UrbanRoutesPage(self.driver)
         phone_number = data.phone_number
         routes_page.add_phone_number(phone_number)
+        # assert
+        current_value = self.driver.find_element(*routes_page.phone_number_input).get_attribute("value") #assert
+        assert current_value == phone_number
 
 
     def test_add_phone_code(self):
         routes_page = UrbanRoutesPage(self.driver)
         routes_page.add_phone_code()
-        phone_number = data.phone_number
+        displayed_phone_number = self.driver.find_element(*routes_page.phone_number_display).text #assert
+        assert displayed_phone_number == data.phone_number, f"Esperado: {data.phone_number}, Actual: {displayed_phone_number}"
 
 # Agregar una tarjeta de crédito.
     def test_add_credit_card(self):
         routes_page = UrbanRoutesPage(self.driver)
         card_number = data.card_number
         cvv = data.card_code
-        routes_page.add_credit_card(card_number, cvv)
+        routes_page.add_credit_card(card_number, cvv)  #assert
+        assert routes_page.verify_credit_card_added()
 
 # Escribir un mensaje para el conductor
     def test_message_to_driver(self):
         routes_page = UrbanRoutesPage(self.driver)
         message_for_driver = data.message_for_driver
         routes_page.message_to_driver(message_for_driver)
+        displayed_message = self.driver.find_element(*routes_page.message_to_driver_field).get_attribute("value")
+        assert displayed_message == message_for_driver #assert
 
 # Pedir una manta y pañuelos.
     def test_counter(self):
@@ -74,11 +79,16 @@ class TestUrbanRoutes:
     def test_order_ice_creams(self):
         routes_page = UrbanRoutesPage(self.driver)
         routes_page.order_ice_creams()
+        ice_cream_counter_element = self.driver.find_element(*routes_page.ice_cream_counter)
+        counter_value = ice_cream_counter_element.text #assert
+        assert counter_value == "2"
+
 # Pedir taxi e informacion
     def test_find_driver(self):
         routes_page = UrbanRoutesPage(self.driver)
         routes_page.click_smart_button()
         WebDriverWait(self.driver, 3)
+
 # Esperar a que aparezca la información del conductor
     def test_wait_driver_information(self):
         routes_page = UrbanRoutesPage(self.driver)
@@ -87,4 +97,5 @@ class TestUrbanRoutes:
     @classmethod
     def teardown_class(cls):
         cls.driver.quit()
+
 
